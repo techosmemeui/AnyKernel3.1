070fe5b139a47f4a22f98e66bbfe6fc33efed18d
Rama Bondan Prakoso <ramanarubp@gmail.com>
2021-08-30 09:58:09 +0700

    ramdisk: fstab: Add workaround for AOSP
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

--- ramdisk/overlay.d/sbin/fstab.sh
+++ ramdisk/overlay.d/sbin/fstab.sh
@@ -8,7 +8,7 @@ if ! grep -v '#' /vendor/etc/fstab.mt6768 | grep -q f2fs; then
     echo "[Genom] data is using F2FS, patching fstab" > /proc/bootprof
     # fstab is missing entry for f2fs, add one
     sed -e "s@/dev/block/platform/bootdevice/by-name/userdata.*@$(cat /vendor/etc/fstab.mt6768 | grep ext4 | grep /data | grep -v '#' | while read a b c d e; do echo $a $b f2fs noatime,nosuid,nodev,discard,reserve_root=32768,resgid=1065,fsync_mode=nobarrier,inlinecrypt latemount,wait,check,quota,formattable,reservedsize=128m,checkpoint=fs,fileencryption=aes-256-xts:aes-256-cts:v1; done)@g" /vendor/etc/fstab.mt6768 | uniq > /dev/fstab.mt6768
-    sed -i "/zram0/d" /dev/fstab.mt6768
+    sed -i "/zram0/d" /dev/fstab.mt6768 && echo "[Genom] Removed zram setup from fstab.mt6768" > /proc/bootprof
     chmod 644 /dev/fstab.mt6768
     mount --bind /dev/fstab.mt6768 /vendor/etc/fstab.mt6768
     chcon u:object_r:vendor_configs_file:s0 /vendor/etc/fstab.mt6768
@@ -20,5 +20,10 @@ if ! grep -v '#' /vendor/etc/fstab.mt6768 | grep -q f2fs; then
     fi
   else
     echo "[Genom] data is using EXT4, patching skipped" > /proc/bootprof
+    cp /vendor/etc/fstab.mt6768 /dev/fstab.mt6768
+    sed -i "/zram0/d" /dev/fstab.mt6768 && echo "[Genom] Removed zram setup from fstab.mt6768" > /proc/bootprof
+    chmod 644 /dev/fstab.mt6768
+    mount --bind /dev/fstab.mt6768 /vendor/etc/fstab.mt6768
+    chcon u:object_r:vendor_configs_file:s0 /vendor/etc/fstab.mt6768
   fi
 fi
