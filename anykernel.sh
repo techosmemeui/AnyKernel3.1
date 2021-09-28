e8ef4cd661d46fbd50aaed26b02daaff8ec7c19d
AL1 <76146196+SeeAli@users.noreply.github.com>
2021-09-28 08:01:04 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,13 +4,13 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Genom Kernel by rama982@telegram
+kernel.string=Niki Kernel by Slebew
 do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=
+device.name1=merlin
 supported.versions=
 supported.patchlevels=
 '; } # end properties
